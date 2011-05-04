2cc7f10de4825fde11103c7b1271b8da577e1f68
Chris Streeter <chris@chrisstreeter.com>
2011-05-04 14:09:02 -0700

    New github.js file.

--- dots/js/github.com.js
+++ dots/js/github.com.js
@@ -1,4 +1,4 @@
 //$('div.news').hide()
 $('div.alert.issues_comment').hide()
 
-$('div.alert.watch_started').css('background-color', '#eee');
+$('div.alert.watch_started').add('div.alert.follow').css('background-color', '#eee');
