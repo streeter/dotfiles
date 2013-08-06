6057f28e3ce38bf19231f8132711ebbaca9c7430
Chris Streeter <chris@chrisstreeter.com>
2013-08-06 10:06:07 -0700

    Allow copy and pasting on apple

---
+++ dots/js/daw.apple.com.js
@@ -0,0 +1,3 @@
+$('[oncut*="return false"]').attr('oncut', 'return true;');
+$('[oncopy*="return false"]').attr('oncopy', 'return true;');
+$('[onpaste*="return false"]').attr('onpaste', 'return true;');
