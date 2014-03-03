de90cbaf318dc33a40ddd291ff340c67100a18ab
Chris Streeter <chris@chrisstreeter.com>
2014-03-03 08:14:18 -0600

    Adjust key repeat rate

--- deploy/defaults/osx.sh
+++ deploy/defaults/osx.sh
@@ -9,6 +9,9 @@ fi
 # General UI/UX                                                               #
 ###############################################################################
 
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
 # Show remaining battery time; hide percentage
 defaults write com.apple.menuextra.battery ShowPercent -string "NO"
 defaults write com.apple.menuextra.battery ShowTime -string "YES"
