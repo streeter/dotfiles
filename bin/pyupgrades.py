b143ae0a9cb3f1f309c3bb030424146997e5c506
Chris Streeter <chris@chrisstreeter.com>
2015-03-23 10:36:51 -0700

    Update pyupgrades script

--- bin/pyupgrades.py
+++ bin/pyupgrades.py
@@ -11,32 +11,35 @@ def version_number_compare(version1, version2):
 def print_status(package, message):
     package_str = '{package.project_name} {package.version}'.format(package=package)
     print '{package:40} {message}'.format(package=package_str, message=message)
-    
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-a', '--all', dest='all', action='store_true', default=False)
     parser.add_argument('-m', '--mirror', dest='mirror', default='http://pypi.python.org/pypi')
-    
+
     args = parser.parse_args()
-    
+
     if not args:
         exit(-1)
-    
+
     pypi = xmlrpclib.ServerProxy(args.mirror)
     for dist in pip.get_installed_distributions():
-        
+
         available = pypi.package_releases(dist.project_name)
         if not available:
             # Try the capitalized package name
             available = pypi.package_releases(dist.project_name.capitalize())
-        
+
         upgrade_available = True
         if not available:
             print_status(dist, 'no releases at pypi')
             continue
-        
-        comparison = version_number_compare(available[0], dist.version)
+
+        try:
+            comparison = version_number_compare(available[0], dist.version)
+        except ValueError:
+            continue
         if comparison == 0:
             if not args.all:
                 continue
