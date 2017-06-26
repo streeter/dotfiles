5ef4e80af1cf2f669b62735a16bb1c447f6547d2
Chris Streeter <chris@chrisstreeter.com>
2017-06-26 09:23:56 -0700

    Updated defaults

--- deploy/defaults/osx.sh
+++ deploy/defaults/osx.sh
@@ -13,12 +13,12 @@ fi
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a really fast key repeat.
-#defaults write NSGlobalDomain InitialKeyRepeat -int 4
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -float 1.98
 
 # Show remaining battery time; hide percentage
-defaults write com.apple.menuextra.battery ShowPercent -string "NO"
-defaults write com.apple.menuextra.battery ShowTime -string "YES"
+defaults write com.apple.menuextra.battery ShowPercent -bool true
+defaults write com.apple.menuextra.battery ShowTime -bool true
 
 # Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -26,9 +26,6 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
 # Expand print panel by default
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-
-# Quit printer app after print jobs complete
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
@@ -44,7 +41,7 @@ defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
 
 # Disable notification of iPad not charging
 echo "Need root to disable iPad not charging notifications..."
-sudo defaults write com.apple.usbd NoiPadNotifications -bool YES
+sudo defaults write com.apple.usbd NoiPadNotifications -bool true
 
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
@@ -68,20 +65,14 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# Disable infrared receiver
-defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -int 0
-
-# Disable AirDrop
-defaults write com.apple.NetworkBrowser DisableAirDrop -bool YES
-
 
 ###############################################################################
 # Screen                                                                      #
 ###############################################################################
 
 # Require password soon after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 30
+defaults write com.apple.screensaver askForPassword -bool true
+defaults write com.apple.screensaver askForPasswordDelay -int 60
 
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
@@ -90,7 +81,7 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 defaults write com.apple.screencapture type -string "png"
 
 # Enable shadow in screenshots
-defaults write com.apple.screencapture disable-shadow -bool false
+defaults write com.apple.screencapture "disable-shadow" -bool false
 
 defaults write com.apple.ScreenSharing debug -bool true
 
@@ -102,15 +93,15 @@ defaults write com.apple.ScreenSharing debug -bool true
 # Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons
 defaults write com.apple.finder QuitMenuItem -bool true
 
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 60
-
 # Show icons for hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Do not show the preview pane
+defaults write com.apple.finder ShowPreviewPane -bool false
 
 # Show all desktop icons
 defaults write com.apple.finder CreateDesktop -bool true
@@ -123,6 +114,11 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Show status bar in Finder
 defaults write com.apple.finder ShowStatusBar -bool true
+
+defaults write com.apple.finder ShowTabView -bool false
+defaults write com.apple.finder ShowSidebar -bool true
+defaults write com.apple.finder SidebarWidth -int 185
+defaults write com.apple.finder NewWindowTargetPath "file:///Users/streeter/"
 
 # Allow text selection in Quick Look
 # Do not enable this. It causes images to stop working.
@@ -146,12 +142,9 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Always open everything in Finder's list view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-
-# Show devices on Desktop
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# Always open everything in Finder's list view
+defaults write com.apple.finder FXPreferredViewStyle "Nlsv"
+defaults write com.apple.finder FXPreferredSearchViewStyle "Nlsv"
 
 # Show icon preview
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showIconPreview true" ~/Library/Preferences/com.apple.finder.plist
@@ -176,50 +169,34 @@ chflags nohidden ~/Library
 # Dock & hot corners                                                          #
 ###############################################################################
 
-# Turn on Dock auto-hiding
-defaults write com.apple.dock autohide -bool true
-
-# Make the Dock appear on the left side
+defaults write com.apple.dock autohide -bool false
+defaults write com.apple.dock magnification -bool false
+defaults write com.apple.dock mineffect "scale"
 defaults write com.apple.dock orientation "left"
-
-# Set the icon size of Dock items to 28 pixels
-defaults write com.apple.dock tilesize -int 28
-
-# Show indicator lights for open applications in the Dock
-defaults write com.apple.dock show-process-indicators -bool true
-
-# Speed up Mission Control animations
-#defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock largesize -int 25
+defaults write com.apple.dock tilesize -int 35
+defaults write com.apple.dock "show-process-indicators" -bool true
 
 # Do not group windows by application in Mission Control
 defaults write com.apple.dock "expose-group-by-app" -bool false
 
-# Enable the 2D Dock
-#defaults write com.apple.dock no-glass -bool true
-
-# Make the Dock 2D Black
-#defaults write com.apple.dock no-glass -int 1
-
 # Make Dock icons of hidden applications translucent
 defaults write com.apple.dock showhidden -bool true
-
-# Enable iTunes track notifications in the Dock
-#defaults write com.apple.dock itunes-notifications -bool true
-
-# Reset Launchpad
-#find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
 
 
 # Hot corners
 # Bottom left screen corner → Mission Control
 defaults write com.apple.dock wvous-bl-corner -int 2
 defaults write com.apple.dock wvous-bl-modifier -int 0
+# Bottom right screen corner → Start screen saver
+defaults write com.apple.dock wvous-br-corner -int 10
+defaults write com.apple.dock wvous-br-modifier -int 0
 # Top left screen corner → Desktop
 defaults write com.apple.dock wvous-tl-corner -int 4
 defaults write com.apple.dock wvous-tl-modifier -int 0
-# Bottom left screen corner → Start screen saver
-#defaults write com.apple.dock wvous-bl-corner -int 5
-#defaults write com.apple.dock wvous-bl-modifier -int 0
+# Top right screen corner → Notifications
+defaults write com.apple.dock wvous-tr-corner -int 12
+defaults write com.apple.dock wvous-tr-modifier -int 0
 
 
 ###############################################################################
@@ -263,15 +240,14 @@ defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 # Address Book, Dashboard, iCal, iTunes, Mail, and Disk Utility               #
 ###############################################################################
 
-# Enable the debug menu in Address Book
 defaults write com.apple.addressbook ABShowDebugMenu -bool true
+defaults write com.apple.addressbook ABBirthDayVisible -bool true
+defaults write com.apple.addressbook ABDefaultAddressCountryCode -string "us"
+defaults write com.apple.addressbook ABNameDisplay -int 0
+defaults write com.apple.addressbook ABNameSortingFormat -string "sortingFirstName sortingLastName"
 
 # Enable the debug menu in iCal
 defaults write com.apple.iCal IncludeDebugMenu -bool true
-
-# Disable send and reply animations in Mail.app
-defaults write com.apple.Mail DisableReplyAnimations -bool true
-defaults write com.apple.Mail DisableSendAnimations -bool true
 
 # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
@@ -280,21 +256,13 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 defaults write com.apple.iTunes invertStoreLinks -bool true
 defaults write com.apple.iTunes show-store-link-arrows -bool false
 
-# Disable the Ping sidebar in iTunes
-defaults write com.apple.iTunes disablePingSidebar -int 1
-
-# Disable all the other Ping stuff in iTunes
-defaults write com.apple.iTunes disablePing -bool true
-defaults write com.apple.iTunes hide-ping-dropdown -bool true
-
-
 # Make ⌘ + F focus the search input in iTunes
 defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Target Search Field" "@F"
 
 # Disable auto-playing when importing
 defaults write com.apple.iTunes play-songs-while-importing -bool FALSE
 
-# Disk Utilty Debug Menu
+# Disk Utility Debug Menu
 defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
 
 # Advanced Disk Utility mode
@@ -305,13 +273,10 @@ defaults write com.apple.diskcopy expert-mode -bool true
 # Terminal                                                                    #
 ###############################################################################
 
+defaults write com.apple.terminal ShowTabBar -bool false
+
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
-
-# Enable “focus follows mouse” for Terminal.app and all X11 apps
-# This means you can hover over a window and start typing in it without clicking first
-#defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true
 
 
 ###############################################################################
@@ -319,18 +284,22 @@ defaults write com.apple.terminal StringEncodings -array 4
 ###############################################################################
 
 defaults write com.apple.dt.Xcode AlwaysShowTabBar -bool false
-defaults write com.apple.dt.Xcode AssistantEditorsLayout -int 0
+defaults write com.apple.dt.Xcode AssistantEditorsLayout -bool false
 defaults write com.apple.dt.Xcode DVTConvertExistingFilesLineEndings -bool true
 defaults write com.apple.dt.Xcode DVTFindBarIgnoreCase -bool true
 defaults write com.apple.dt.Xcode DVTTextAlignConsecutiveSlashSlashComments -bool true
+defaults write com.apple.dt.Xcode DVTTextAutoSuggestCompletions -bool true
 defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool true
+defaults write com.apple.dt.Xcode DVTTextIndentTabWidth -int 4
+defaults write com.apple.dt.Xcode DVTTextIndentWidth -int 4
 defaults write com.apple.dt.Xcode DVTTextPageGuideLocation -int 100
+defaults write com.apple.dt.Xcode DVTTextShowFoldingSidebar -bool true
 defaults write com.apple.dt.Xcode DVTTextShowLineNumbers -bool true
 defaults write com.apple.dt.Xcode DVTTextShowPageGuide -bool true
 defaults write com.apple.dt.Xcode IDEBuildOperationMaxNumberOfConcurrentCompileTasks `sysctl -n hw.ncpu`
 defaults write com.apple.dt.Xcode IDECodeCompletionFuzzyMode 3
 defaults write com.apple.dt.Xcode IDEDisableGitSupportForNewProjects -bool true
-defaults write com.apple.dt.Xcode IDEWorkaroundForRadar6288283 3
+defaults write com.apple.dt.Xcode IDEDocViewerPreferredSDK "iphoneos"
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
 defaults write com.apple.dt.Xcode ShowDVTDebugMenu -bool false
 
@@ -339,27 +308,14 @@ defaults write com.apple.dt.Xcode OMOpenInDashStyle -int 2
 
 defaults write com.apple.iphonesimulator SBFakeCarrier "Streeter"
 
-###############################################################################
-# Quicktime                                                                   #
-###############################################################################
-
-# Do not restore last active video
-defaults write com.apple.QuickTimePlayerX NSQuitAlwaysKeepsWindows -bool false
-
 
 ###############################################################################
 # Photos / Image Capture                                                      #
 ###############################################################################
 
 # Never auto-launch photos or image capture for new devices
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
-###############################################################################
-# Preview                                                                     #
-###############################################################################
-
-# Do not restore last active document
-defaults write com.apple.Preview NSQuitAlwaysKeepsWindows -bool false
 
 ###############################################################################
 # Time Machine                                                                #
@@ -374,40 +330,8 @@ defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 ###############################################################################
 
 defaults write com.tapbots.TweetbotMac ShowTheTweetbots -bool true
-defaults write com.tapbots.TweetbotMac TextAutoCorrect -bool YES
-defaults write com.tapbots.TweetbotMac TextAutomaticQuoteSubstitution -bool YES
-defaults write com.tapbots.TweetbotMac TextAutomaticTextReplacement -bool YES
-defaults write com.tapbots.TweetbotMac TextContinuousSpellChecking -bool YES
-defaults write com.tapbots.TweetbotMac displayNameType -int 3
-defaults write com.tapbots.TweetbotMac fontSize -int 13
-defaults write com.tapbots.TweetbotMac quoteFormatType -int 1
-defaults write com.tapbots.TweetbotMac openURLInBackground -bool YES
+defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
 
-
-###############################################################################
-# Twitter.app                                                                 #
-###############################################################################
-
-# Disable smart quotes as it’s annoying for code tweets
-defaults write com.twitter.twitter-mac AutomaticQuoteSubstitutionEnabled -bool false
-
-# Show the app window when clicking the menu icon
-defaults write com.twitter.twitter-mac MenuItemBehavior -int 1
-
-# Enable the hidden ‘Develop’ menu
-defaults write com.twitter.twitter-mac ShowDevelopMenu -bool true
-
-# Open links in the background
-defaults write com.twitter.twitter-mac openLinksInBackground -bool true
-
-# Allow closing the ‘new tweet’ window by pressing `Esc`
-defaults write com.twitter.twitter-mac ESCClosesComposeWindow -bool true
-
-# Show full names rather than Twitter handles
-defaults write com.twitter.twitter-mac ShowFullNames -bool true
-
-# The compose window shouldn't float over everything
-defaults write com.twitter.twitter-mac NormalComposeWindowLevel -bool true
 
 ###############################################################################
 # Kill affected applications                                                  #
