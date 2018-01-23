684868d13a0567dd8468b4953195af8690900f80
Chris Streeter <chris@chrisstreeter.com>
2018-01-23 14:06:06 -0800

    Remove some unused things

--- dots/js/daw.apple.com.js
+++
@@ -1,3 +0,0 @@
-$('[oncut*="return false"]').attr('oncut', 'return true;');
-$('[oncopy*="return false"]').attr('oncopy', 'return true;');
-$('[onpaste*="return false"]').attr('onpaste', 'return true;');
